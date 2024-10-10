dfa8b01ce45964b2f8ae23ac53412c1751a972cf
Bairon Naranjo <bnaranjo@bancolombia.com.co>
2024-10-10 07:57:14 -0500

    Entrega de taller #3

--- aplicacionbancaria/src/main/java/com/bancolombia/aplicacionbancaria/config/WebConfig.java
+++ aplicacionbancaria/src/main/java/com/bancolombia/aplicacionbancaria/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.bancolombia.aplicacionbancaria.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,8 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Autowired
     private AuditoriaInterceptor auditoriaInterceptor;
+
+    public WebConfig(AuditoriaInterceptor auditoriaInterceptor) {
+        this.auditoriaInterceptor = auditoriaInterceptor;
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
